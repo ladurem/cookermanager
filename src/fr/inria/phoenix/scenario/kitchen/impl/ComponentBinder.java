0a8f60bb40d685e0603be45a54da422b74883665
ladurem <mladure@enseirb-matmeca.fr>
2015-01-05 15:51:43 +0100

    First Commit

---
+++ src/fr/inria/phoenix/scenario/kitchen/impl/ComponentBinder.java
@@ -0,0 +1,45 @@
+package fr.inria.phoenix.scenario.kitchen.impl;
+        
+import fr.inria.phoenix.diasuite.framework.context.kitchencontext.AbstractKitchenContext;
+import fr.inria.phoenix.diasuite.framework.context.kitchenstatus.AbstractKitchenStatus;
+import fr.inria.phoenix.diasuite.framework.context.lastmove.AbstractLastMove;
+import fr.inria.phoenix.diasuite.framework.context.lastmovebehindkitchen.AbstractLastMoveBehindKitchen;
+import fr.inria.phoenix.diasuite.framework.controller.kitchencontroller.AbstractKitchenController;
+import fr.inria.phoenix.diasuite.framework.misc.AppComponentBinder;
+
+/* (non-Javadoc)
+ * The binder to provides the various components of the application
+ * @see fr.inria.phoenix.diasuite.framework.misc.AppComponentBinder
+ */
+public class ComponentBinder extends AppComponentBinder {
+
+	@Override
+	public Class<? extends AbstractKitchenContext> getKitchenContextClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Class<? extends AbstractKitchenStatus> getKitchenStatusClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Class<? extends AbstractLastMove> getLastMoveClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Class<? extends AbstractLastMoveBehindKitchen> getLastMoveBehindKitchenClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Class<? extends AbstractKitchenController> getKitchenControllerClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
